2f88e378b6454782632f17ec5e40c53cb9370e0f
Brandon Herrera <brandondh769@gmail.com>
2026-04-03 13:22:57 -0600

    Add new image for the hero caroussel

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-let i = 1;
-const images = 9;
+let i = 0;
+const images = 10;
 const hero = document.querySelector('.hero');
 
-const colors = ['#DCE1FF', '#E1F0F4', '#d3d2d8', '#b7d4e3', '#b5cfce', '#f0e8da', '#e3c4c5', '#d2d8eb', '#c6c8d0']
+const colors = ['#DCE1FF', '#E1F0F4', '#d3d2d8', '#b7d4e3', '#b5cfce', '#f0e8da', '#e3c4c5', '#d2d8eb', '#c6c8d0', '#d3d9af'];
 
 setInterval(() => {
-  hero.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('images/background/background (${i}).jpg')`;
-  
-  document.documentElement.style.setProperty('--Material-Blue-Light', colors[i-1]);
-  console.log(colors[i-1]);
-  console.log(i);
+    i = (i % images) + 1;
 
-  i = (i % images) + 1;
+    hero.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('images/background/background (${i}).jpg')`;
 
+    document.documentElement.style.setProperty('--accent-text', colors[i-1]);
 }, 6000);
 
 const html = document.documentElement;
